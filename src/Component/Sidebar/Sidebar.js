434a2385938108583f757a4dbb9a6aa657ffafe7
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-06-24 17:40:54 +0530

    added sidebar footer and completed sidebar section

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import SidebarOptions from "./SidebarOptions";
 import InboxIcon from "@mui/icons-material/Inbox";
@@ -10,6 +10,9 @@ import LabelImportantIcon from "@mui/icons-material/LabelImportant";
 import NearMeIcon from "@mui/icons-material/NearMe";
 import NoteIcon from "@mui/icons-material/Note";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const Sidebar = () => {
   return (
@@ -25,6 +28,20 @@ const Sidebar = () => {
       <SidebarOptions Icon={NearMeIcon} title="Sent" number={5} />
       <SidebarOptions Icon={NoteIcon} title="Drafts" number={2} />
       <SidebarOptions Icon={ExpandMoreIcon} title="More" number={26} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
